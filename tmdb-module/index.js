ad4e6175c260e89333a7c983c8104d1eeab79e19
Randy Yin <randyyin28@gmail.com>
2019-03-25 14:30:18 -0700

    Update index.js

--- tmdb-module/index.js
+++ tmdb-module/index.js
@@ -1,17 +1,17 @@
-const
-    config = require('./config'),
-    superagent = require('superagent')
+const config = require('./config')
+const superagent = require('superagent')
 
 const _fetch = (command) => {
     return superagent.get(`${config.url}/${command}`)
         .then(response => response.body)
-        .catch(error => error.response.body)
 }
 
 exports.search = (sq) => {
     return _fetch(`search/tv?api_key=c261880f23d34ae50288c921b209df51&query=${sq}`)
 }
 
-exports.information = (id) => {
+exports.fetch_by_id = (id) => {
     return _fetch(`tv/${id}?api_key=c261880f23d34ae50288c921b209df51`)
 }
+
+
