6fbc498c385faa24d389b4d5c5b49cb7a9978eeb
Randy Yin <randyyin28@gmail.com>
2019-03-25 14:32:53 -0700

    Update app.js

--- tmdb-app/app.js
+++ tmdb-app/app.js
@@ -1,84 +1,83 @@
-const
+const 
     tmdb = require('tmdb-module'),
-    inquirer = require('inquirer'),
-    superagent = require('superagent')
+    inquirer = require('inquirer')
 
-const searchWithParams = (sq) => {
-
-    tmdb.search(sq)
-        .then(results => {
-
-            let available = []
-            results.array.forEach(element => {
-                available.push(`${element.show.name} (${element.show.id})`)
-            });
-
-            inquirer.prompt([{
-                type:'list',
-                message: 'Select a show to view its details',
-                name:'showName',
-                choices:available
-            }])
-            .then(choice => {
-                console.log(`\n${choice.ShowName}\n`)
-                let selection = choice.ShowName.split("(")[1].slice(0,-1)
-                tmdb.fetch_by_id(selection)
-                .then(showObject => {
-                    display(showObject)
-                })
-            })
-            .catch(err => console.log(err))
-        })
-        .catch(err => console.log(err))
+async function searchPrompt(newshowList) {
+    return inquirer.prompt([{
+        type: 'list',
+        message: 'Please select the tv show you are searching for.',
+        name: 'show',
+        choices: newshowList,
+        validate: show => {
+            if (show.length == 0) 
+                return 'You must select a show!'
+            else 
+                return true
+        }
+    }])
 }
 
-const singleSearch = (ss) => {
-    tmdb.singleSearch(ss)
-    .then(res => {
-        display(res);
+async function search(name) {
+
+    const newshowList = []
+
+    const tvshow = await tmdb.search(name)
+    
+    const showlist = tvshow.results
+
+    // console.log(showlist)
+
+    showlist.forEach(function(element) {
+        newshowList.push(element.name)
     })
-    .catch(err => console.log(err))
+
+    // console.log(newshowList)
+
+    const foundShow = await searchPrompt(newshowList)
+
+    // console.log(foundShow)
+
+   //get value of show property. Example = { show: 'Friends' }
+   const nameOfShowString = foundShow.show
+  
+   let showID = 0;
+
+    for (let i = 0; i < showlist.length; i++ ) {
+        if ( nameOfShowString === `${showlist[i].name}` ) {
+            showID = showlist[i].id
+            // console.log(showID)
+            break
+        }
+    }
+    
+    const showInfo = await tmdb.fetch_by_id(showID)
+
+    // console.log(showInfo)
+
+    print(showInfo)
 }
 
-function display(show){
-    console.log(`Name: ${show.name}`)
+const print = (showInfo) => {
+    // console.log(showInfo)
 
+    console.log('==========SHOW INFORMATION==========')
 
-    rl.question('Would you like to view show\'s episodes? [Y/N]', (answer) => {
-      if(answer.toLowerCase() === 'y'){
-         showSeasons(show.id)
-      }
-      rl.close();
-    });
+    console.log(`Show Name: ${showInfo.name}`)
+    console.log('')
+    console.log(`Popularity: ${showInfo.popularity}`)
+    console.log('')
+    console.log(`Original Language: ${showInfo.original_language}`)
+    console.log('')
+    console.log(`Country of Origin: ${showInfo.origin_country}`)
+    console.log('')
+    console.log(`First Air Date: ${showInfo.first_air_date}`)
+    console.log('')
+    console.log(`Overview: ${showInfo.overview}`)
+    console.log('')
+
+    console.log('===============END===============')
 }
 
 module.exports = {
-    searchWithParams
+    search
 }
-
-
-
-
-
-
-// async function searchPrompt(matches) {
-//     return inquirer.prompt([{
-//         type:'list',
-//         message:"select the tv show you are searching for",
-//         name: 'tv',
-//         choices: matches,
-//         validate: tv => {
-//             if (tv.length == 0) {
-//                 return 'You must select a tv show'
-//             } else {
-//                 return true;
-//             }
-//         }
-//     }])
-// }
-
-// async function search() {
-    
-// }
-
-    
